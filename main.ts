dea8b231e2cae317c835fa61689ee5877c238113
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-12 17:53:25 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,308 +1,104 @@
-// MakeCode blocks for Infrared
-// Original Code by 劉正吉 https://github.com/lioujj/pxt-IR
-// MIT License
-// Changes for Calliope Mini by M. Klein
-//
-//% weight=0 color=#87bc4b icon="\uf1eb" block="Test 123"
-namespace IR {
-    export enum encodingType {
-        //% block="NEC"
-        NEC,
-        //% block="SONY"
-        SONY
-    }
-    let tempHandler: Action;
-    let irLed = AnalogPin.C16; // P16 -> C16 Änderung für Calliope Mini
-    const pwmPeriod = 26;
-    pins.analogWritePin(irLed, 0);
-    pins.analogSetPeriod(irLed, pwmPeriod);
-    let send_init = false;
-    let rec_init = false;
-    let arr: number[] = []
-    let received = false
-    let first = true
-    let rec_Type = ""
-    let messageStr = ""
-    let recPin = DigitalPin.C17 // P8 -> C17 Änderung für Calliope Mini
-    let thereIsHandler = false
-    arr = []
+ //% block="mouseBot-Motoren"
+//% color="#0064A2"
+enum mouseBotMotors {
+    //% block=Links
+    Links,
+    //% block=Rechts
+    Rechts
+}
 
-    function transmitBit(highTime: number, lowTime: number): void {
-        pins.analogWritePin(irLed, 512);
-        control.waitMicros(highTime);
-        pins.analogWritePin(irLed, 0);
-        control.waitMicros(lowTime);
-    }
+//% block="mouseBot-Linienfolger"
+//% color="#0064A2"
+enum mouseBotLineFollowers {
+    //% block=Links
+    Links,
+    //% block=Rechts
+    Rechts
+}
 
-    /**
-     *  set the infrared LED pin.
-     */
-    //% blockId=setIR_pin block="Test123 %myPin" blockExternalInputs=false
-    //% weight=90 blockGap=10
-    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
-    //% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
-    export function setIR_pin(myPin: AnalogPin) {
-        irLed = myPin;
-        pins.analogWritePin(irLed, 0);
-        pins.analogSetPeriod(irLed, pwmPeriod);
-        send_init = true;
-    }
+//% block="LED-Farben"
+//% color="#0064A2"
+enum colors {
+    //% block=Rot
+    Red = 0xFF0000,
+    //% block=Orange
+    Orange = 0xFFA500,
+    //% block=Gelb
+    Yellow = 0xFFFF00,
+    //% block=Grün
+    Green = 0x00FF00,
+    //% block=Blau
+    Blue = 0x0000FF,
+    //% block=Indigo
+    Indigo = 0x4b0082,
+    //% block=Violet
+    Violet = 0x8a2be2,
+    //% block=Pink
+    Purple = 0xFF00FF,
+    //% block=Weiss
+    White = 0xFFFFFF,
+    //% block=Aus
+    Black = 0x000000
+}
 
-    /**
-     *  set the IR receiver pin.
-     */
-    //% blockId=setREC_pin block="Infrarotempfänger an Pin %myPin" blockExternalInputs=false
-    //% weight=85 blockGap=10
-    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
-    //% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
-    export function setREC_pin(myPin: DigitalPin) {
-        recPin = myPin;
-        pins.setEvents(recPin, PinEventType.Pulse)
-        pins.setPull(recPin, PinPullMode.PullUp)
-        pins.onPulsed(recPin, PulseValue.Low, function () {
-            arr.push(input.runningTimeMicros())
-        })
-        pins.onPulsed(recPin, PulseValue.High, function () {
-            arr.push(input.runningTimeMicros())
-        })
-        control.onEvent(recPin, DAL.MICROBIT_PIN_EVENT_ON_TOUCH, tempHandler);
-        rec_init = true;
-    }
-
-    /**
-     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
-     */
-    //% blockId=sendMyMessage1 block="sende IR-Code %msg| ,%times| mal, mit der Codierung %myType"
-    //% weight=80 blockGap=10
-    export function sendMyMessage1(msg: string, times: number, myType: encodingType): void {
-        if (send_init) {
-            //control.inBackground(() => {
-            sendMessage(convertHexStrToNum(msg), times, myType);
-            //})
-        }
-    }
-    /**
-     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
-     */
-    //% blockId=sendMyMessage2 block="sende IR-Code %msg| ,%times| mal, mit der Codierung %myType"
-    //% weight=75 blockGap=10
-    export function sendMyMessage2(msg: string, times: number, myType: string): void {
-        if (send_init) {
-            if (myType == "NEC") {
-                sendMessage(convertHexStrToNum(msg), times, encodingType.NEC);
-            } else if (myType == "SONY") {
-                sendMessage(convertHexStrToNum(msg), times, encodingType.SONY); 
-            }
-        }
-    }
+//% color="#0064A2"
+namespace mouseBot {
 
 
-    function encode(myCode: number, bits: number, trueHigh: number, trueLow: number, falseHigh: number, falseLow: number): void {
-        const MESSAGE_BITS = bits;
-        for (let mask = 1 << (MESSAGE_BITS - 1); mask > 0; mask >>= 1) {
-            if (myCode & mask) {
-                transmitBit(trueHigh, trueLow);
-            } else {
-                transmitBit(falseHigh, falseLow);
-            }
-        }
+    //% block="Motor %motor mit Geschwindigkeit %velocity"
+    //% velocity.min=-100 velocity.max= 100 velocity.defl=30
+    export function MotorX(motor: mouseBotMotors, velocity: number) {
+
+    }
+    //% block="Beide Motoren mit Geschwindigkeit %velocity"
+    //% velocity.min=-100 velocity.max= 100 velocity.defl=30
+    export function MotorLeftandRight(velocity: number) {
+
+    }
+    //% block="Beide Motoren auslaufen lassen"
+    export function MotorLeftandRightRunDown() {
+
+    }
+    //% block="Beide Motoren stoppen"
+    export function MotorLeftandRightStop() {
+
+    }
+    //% block="Ultraschallsensor-Distanz (in cm)"
+    export function GetUltrasonicDist(): number {
+        let dist = 0
+        return dist;
+    }  
+
+    //% block="Setze Farbe von allen RGB-LED auf %color"
+    //% color.defl=Red
+    export function showColor(color:colors) {
+    } 
+
+    //% block="Setze Farbe von RGB-LED %ledNumber auf %color"
+    //% ledNumber.min=0 ledNumber.max= 6 color.defl=Red
+    export function setColorOfLed(ledNumber: number,color:colors) {
+    } 
+    //% block="Setze Helligkeit von RGB-LED %ledNumber auf %brightness"
+    //% ledNumber.min=0 ledNumber.max= 6 brightness.min=0 brightness.max=100 brightness.defl=20
+    export function setBrightnessOfLed(ledNumber: number,brightness: number) {
+    } 
+
+    //% block="Zeige Regenbogen von Farbton %startC bis %endC"
+    //% shift.defl=1
+    export function showRainbow(startC: number,endC: number) {
+    }
+    //% block="Verschiebe RGB-LEDs um %shift"
+    //% shift.defl=1
+    export function shiftLEDs(shift: number) {
+    }
+    //% block="Rotiere RGB-LEDs um %rotate"
+    //% rotate.defl=1
+    export function rotateLEDs(rotate: number) {
     }
 
-    function sendNEC(message: number, times: number): void {
-        const enum NEC {
-            startHigh = 9000,
-            startLow = 4500,
-            stopHigh = 560,
-            stopLow = 0,
-            trueHigh = 560,
-            trueLow = 1690,
-            falseHigh = 560,
-            falseLow = 560,
-            interval = 110000
-        }
-        //let address = Math.idiv(message, 0x010000)
-        let address = message >> 16;
-        let command = message % 0x010000;
-        const MESSAGE_BITS = 16;
-        let startTime = 0;
-        let betweenTime = 0;
-        for (let sendCount = 0; sendCount < times; sendCount++) {
-            startTime = input.runningTimeMicros();
-            transmitBit(NEC.startHigh, NEC.startLow);
-            encode(address, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
-            encode(command, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
-            transmitBit(NEC.stopHigh, NEC.stopLow);
-            betweenTime = input.runningTimeMicros() - startTime
-            if (times > 0)
-                control.waitMicros(NEC.interval - betweenTime);
-        }
-    }
-
-    function sendSONY(message: number, times: number): void {
-        const enum SONY {
-            startHigh = 2300,
-            startLow = 500,
-            trueHigh = 1100,
-            trueLow = 500,
-            falseHigh = 500,
-            falseLow = 500,
-            interval = 45000
-        }
-        const MESSAGE_BITS = 12;
-        let startTime = 0;
-        let betweenTime = 0;
-        for (let sendCount = 0; sendCount < times; sendCount++) {
-            startTime = input.runningTimeMicros();
-            transmitBit(SONY.startHigh, SONY.startLow);
-            encode(message, 12, SONY.trueHigh, SONY.trueLow, SONY.falseHigh, SONY.falseLow);
-            betweenTime = input.runningTimeMicros() - startTime
-            if (times > 0)
-                control.waitMicros(SONY.interval - betweenTime);
-        }
-    }
-
-    export function sendMessage(message: number, times: number, myType: encodingType): void {
-        switch (myType) {
-            case encodingType.NEC: sendNEC(message, times);
-            case encodingType.SONY: sendSONY(message, times);
-            default: sendNEC(message, times);
-        }
-    }
-
-    function convertHexStrToNum(myMsg: string): number {
-        let myNum = 0
-        for (let i = 0; i < myMsg.length; i++) {
-            if ((myMsg.charCodeAt(i) > 47) && (myMsg.charCodeAt(i) < 58)) {
-                myNum += (myMsg.charCodeAt(i) - 48) * (16 ** (myMsg.length - 1 - i))
-            } else if ((myMsg.charCodeAt(i) > 96) && (myMsg.charCodeAt(i) < 103)) {
-                myNum += (myMsg.charCodeAt(i) - 87) * (16 ** (myMsg.length - 1 - i))
-            } else if ((myMsg.charCodeAt(i) > 64) && (myMsg.charCodeAt(i) < 71)) {
-                myNum += (myMsg.charCodeAt(i) - 55) * (16 ** (myMsg.length - 1 - i))
-            } else {
-                myNum = 0
-                break
-            }
-        }
-        return myNum
-    }
-
-    //------------------receiver-------------
-
-    function resetReceiver() {
-        arr = []
-        received = false
-    }
-
-    control.inBackground(function () {
-        basic.forever(function () {
-            if ((!received) && (rec_init)) {
-                if (arr.length > 20) {
-                    if ((input.runningTimeMicros() - arr[arr.length - 1]) > 120000) {
-                        if (first) {
-                            resetReceiver()
-                            first = false
-                        } else {
-                            received = true
-                            decodeIR();
-                        }
-                    }
-                }
-            }
-        })
-    })
-
-    function decodeIR() {
-        let addr = 0
-        let command = 0
-        messageStr = ""
-        rec_Type = ""
-        for (let i = 0; i <= arr.length - 1 - 1; i++) {
-            arr[i] = arr[i + 1] - arr[i]
-        }
-        if (((arr[0] + arr[1]) > 13000) && ((arr[0] + arr[1]) < 14000)) {
-            rec_Type = "NEC"
-            arr.removeAt(1)
-            arr.removeAt(0)
-            addr = pulseToDigit(0, 15, 1600)
-            command = pulseToDigit(16, 31, 1600)
-            messageStr = convertNumToHexStr(addr, 4) + convertNumToHexStr(command, 4)
-            arr = [];
-            if (thereIsHandler) {
-                tempHandler();
-            }
-        } else if (((arr[0] + arr[1]) > 2600) && ((arr[0] + arr[1]) < 3200)) {
-            rec_Type = "SONY"
-            arr.removeAt(1)
-            arr.removeAt(0)
-            command = pulseToDigit(0, 11, 1300)
-            messageStr = convertNumToHexStr(command, 3)
-            arr = [];
-            if (thereIsHandler) {
-                tempHandler();
-            }
-        }
-        resetReceiver();
-    }
-
-    function pulseToDigit(beginBit: number, endBit: number, duration: number): number {
-        let myNum = 0
-        for (let i = beginBit; i <= endBit; i++) {
-            myNum <<= 1
-            if ((arr[i * 2] + arr[i * 2 + 1]) < duration) {
-                myNum += 0
-            } else {
-                myNum += 1
-            }
-        }
-        return myNum
-    }
-
-    function convertNumToHexStr(myNum: number, digits: number): string {
-        let tempDiv = 0
-        let tempMod = 0
-        let myStr = ""
-        tempDiv = myNum
-        while (tempDiv > 0) {
-            tempMod = tempDiv % 16
-            if (tempMod > 9) {
-                myStr = String.fromCharCode(tempMod - 10 + 97) + myStr
-            } else {
-                myStr = tempMod + myStr
-            }
-            tempDiv = tempDiv / 16  // Math.idiv(tempDiv, 16) geändert wegen Calliope V0-Core
-        }
-        while (myStr.length != digits) {
-            myStr = "0" + myStr
-        }
-        return myStr
-    }
-
-    /**
-     * Do something when a receive IR
-     */
-    //% blockId=onReceivedIR block="wenn IR Code empfangen" blockInlineInputs=true
-    //% weight=70 blockGap=10
-    export function onReceivedIR(handler: Action): void {
-        tempHandler = handler
-        thereIsHandler = true
-    }
-
-    /**
-     * return the encoding type of the received IR 
-     */
-    //% blockId=getRecType block="die empfangene IR Codierung"
-    //% weight=60 blockGap=10
-    export function getRecType(): string {
-        return rec_Type
-    }
-
-    /**
-     * return the message of the received IR 
-     */
-    //% blockId=getMessage block="die empfangene IR Nachricht"
-    //% weight=60 blockGap=10
-    export function getMessage(): string {
-        return messageStr
-    }
-
-} 
+    //% block="Farbe von Linienfolger %lineFollower"
+    export function getColorOfLinefollower(lineFollower: mouseBotLineFollowers): colors {
+        let color= colors.Red
+        return color;
+    }    
+}
