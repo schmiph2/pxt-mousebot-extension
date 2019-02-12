25d27ffac77ed18168c394f0075f08c588e00efc
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-12 15:51:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color="#0064A2"
 namespace mousebot {
     //% block="Addiere %a mit %b"
-    export function addAB(a: number, b: number): number {
+    export function addaundb(a: number, b: number): number {
        let c=a+b
        return c;
     }
