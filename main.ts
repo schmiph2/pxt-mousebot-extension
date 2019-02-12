6c1e51f6b8de20e1f479b49057528cbc3a332f89
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-12 15:54:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color="#0064A2"
+//% weight=0 color="#0064A2" icon="\uf1eb" block="mouseBot"
 namespace mousebot {
     //% block="Addiere %a mit %b"
     export function addaundb(a: number, b: number): number {
