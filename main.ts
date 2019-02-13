86659e6c3c9304d008f0dbe807a03195ef32d226
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-13 15:41:55 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,97 +1,50 @@
-//% block="mouseBot-Motoren"
-//% color="#0064A2"
-enum mouseBotMotors {
-    //% block=Links
-    Links,
-    //% block=Rechts
-    Rechts
-}
-
-//% block="mouseBot-Linienfolger"
-//% color="#0064A2"
-enum mouseBotLineFollowers {
-    //% block=Links
-    Links,
-    //% block=Rechts
-    Rechts
-}
-
-//% block="LED-Farben"
-//% color="#0064A2"
-enum colorsLEDs {
-    //% block=Rot
-    Red = 0xFF0000,
-    //% block=Orange
-    Orange = 0xFFA500,
-    //% block=Gelb
-    Yellow = 0xFFFF00,
-    //% block=Grün
-    Green = 0x00FF00,
-    //% block=Blau
-    Blue = 0x0000FF,
-    //% block=Indigo
-    Indigo = 0x4b0082,
-    //% block=Violet
-    Violet = 0x8a2be2,
-    //% block=Pink
-    Purple = 0xFF00FF,
-    //% block=Weiss
-    White = 0xFFFFFF,
-    //% block=Aus
-    Black = 0x000000
-}
-//% weight=5 color=#0064A2 icon="\uf110"
-namespace mouseBot {
-
-
-    //% block="Motor %motor mit Geschwindigkeit %velocity"
-    //% velocity.min=-100 velocity.max= 100 velocity.defl=30
-    export function MotorX(motor: mouseBotMotors, velocity: number) {
-
+// MakeCode blocks for Infrared
+// Original Code by 劉正吉 https://github.com/lioujj/pxt-IR
+// MIT License
+// Changes for Calliope Mini by M. Klein
+//
+//% weight=0 color=#87bc4b icon="\uf1eb" block="Test 2345"
+namespace IR {
+    export enum encodingType {
+        //% block="NEC"
+        NEC,
+        //% block="SONY"
+        SONY
     }
-    //% block="Beide Motoren mit Geschwindigkeit %velocity"
-    //% velocity.min=-100 velocity.max= 100 velocity.defl=30
-    export function MotorLeftandRight(velocity: number) {
+    let tempHandler: Action;
+    let irLed = AnalogPin.C16; // P16 -> C16 Änderung für Calliope Mini
+    const pwmPeriod = 26;
+    pins.analogWritePin(irLed, 0);
+    pins.analogSetPeriod(irLed, pwmPeriod);
+    let send_init = false;
+    let rec_init = false;
+    let arr: number[] = []
+    let received = false
+    let first = true
+    let rec_Type = ""
+    let messageStr = ""
+    let recPin = DigitalPin.C17 // P8 -> C17 Änderung für Calliope Mini
+    let thereIsHandler = false
+    arr = []
 
+    function transmitBit(highTime: number, lowTime: number): void {
+        pins.analogWritePin(irLed, 512);
+        control.waitMicros(highTime);
+        pins.analogWritePin(irLed, 0);
+        control.waitMicros(lowTime);
     }
-    //% block="Beide Motoren auslaufen lassen"
-    export function MotorLeftandRightRunDown() {
 
+    /**
+     *  set the infrared LED pin.
+     */
+    //% blockId=setIR_pin block="Test123 %myPin" blockExternalInputs=false
+    //% weight=90 blockGap=10
+    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
+    //% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
+    export function setIR_pin123(myPin: AnalogPin) {
+        irLed = myPin;
+        pins.analogWritePin(irLed, 0);
+        pins.analogSetPeriod(irLed, pwmPeriod);
+        send_init = true;
     }
-    //% block="Beide Motoren stoppen"
-    export function MotorLeftandRightStop() {
-
-    }
-    //% block="Ultraschallsensor-Distanz (in cm)"
-    export function GetUltrasonicDist(): number {
-        let dist = 0
-        return dist;
-    }  
-
-    //% block="Setze Farbe von allen RGB-LED auf %colorLEDs"
-    //% color.defl=Red
-    export function showColor(colorLEDs:colorsLEDs) {
-    } 
-
-    //% block="Setze Farbe von RGB-LED %ledNumber auf %ledColor"
-    //% ledNumber.min=0 ledNumber.max= 6 color.defl=Red
-    export function setColorOfLed(ledNumber: number,ledColor:colorsLEDs) {
-    } 
-    //% block="Setze Helligkeit von RGB-LED %ledNumber auf %ledBrightness"
-    //% ledNumber.min=0 ledNumber.max= 6 brightness.min=0 brightness.max=100 brightness.defl=20
-    export function setBrightnessOfLed(ledNumber: number,ledBrightness: number) {
-    } 
-
-    //% block="Zeige Regenbogen von Farbton %startC bis %endC"
-    //% shift.defl=1
-    export function showRainbow(startC: number,endC: number) {
-    }
-    //% block="Verschiebe RGB-LEDs um %ledsShift"
-    //% shift.defl=1
-    export function shiftLEDs(ledsShift: number) {
-    }
-    //% block="Rotiere RGB-LEDs um %ledsRotate"
-    //% rotate.defl=1
-    export function rotateLEDs(ledsRotate: number) {
-    }   
-}
+}    
