e06ee7be247f1f8a3bed559b25edae154f31193c
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-13 17:56:23 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,11 @@
-// MakeCode blocks for Infrared
-// Original Code by 劉正吉 https://github.com/lioujj/pxt-IR
-// MIT License
-// Changes for Calliope Mini by M. Klein
-//
-//% weight=0  color="#0064A2" icon="\uf544" block="mouseBot"
-namespace IR {
-    export enum encodingType {
-        //% block="NEC"
-        NEC,
-        //% block="SONY"
-        SONY
-    }
-    let tempHandler: Action;
-    let irLed = AnalogPin.C16; // P16 -> C16 Änderung für Calliope Mini
-    const pwmPeriod = 26;
-    pins.analogWritePin(irLed, 0);
-    pins.analogSetPeriod(irLed, pwmPeriod);
-    let send_init = false;
-    let rec_init = false;
-    let arr: number[] = []
-    let received = false
-    let first = true
-    let rec_Type = ""
-    let messageStr = ""
-    let recPin = DigitalPin.C17 // P8 -> C17 Änderung für Calliope Mini
-    let thereIsHandler = false
-    arr = []
 
-    function transmitBit(highTime: number, lowTime: number): void {
-        pins.analogWritePin(irLed, 512);
-        control.waitMicros(highTime);
-        pins.analogWritePin(irLed, 0);
-        control.waitMicros(lowTime);
-    }
-     //% block="Beide Motoren mit Geschwindigkeit %velocity"
+//% weight=0  color="#0064A2" icon="\uf544" block="mouseBot"
+namespace MB {
+    //% blockId=motorleftright block="Beide Motoren mit Geschwindigkeit %velocity"
+    //% weight=90 blockGap=10 
     //% velocity.min=-100 velocity.max= 100 velocity.defl=30
     export function MotorLeftandRight(velocity: number) {
-        
+        pins.analogWritePin(AnalogPin.C16, velocity);
     }
 
     /**
