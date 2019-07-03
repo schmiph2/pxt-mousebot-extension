72f13dac9b3d501768440aa11fa9b1c125785533
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-07-03 16:42:31 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -85,14 +85,11 @@ namespace motors {
         /**
      * Draws an image on the LED screen.
      * @param leds the pattern of LED to turn on/off
-     * @param interval time in milliseconds to pause after drawing
      */
-    //% help=basic/show-leds
     //% weight=95 blockGap=8
     //% imageLiteral=1 async
     //% blockId=device_show_leds
     //% block="show leds" icon="\uf00a"
-    //% parts="ledmatrix"
     export function showLED(ImageLiteral leds) {
         pins.analogWritePin(AnalogPin.C16, 0);
     }
